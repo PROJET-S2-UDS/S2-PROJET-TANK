0bbe1ffa9ca71bdf46b749b0220cbf3dfe9eabd4
William Roberge <202059891@edu.clg.qc.ca>
2023-02-15 11:58:01 -0500

    remove

--- S2-PROJET-TANK-2/S2-PROJET-TANK.cpp
+++ S2-PROJET-TANK-2/S2-PROJET-TANK.cpp
@@ -1,6 +1,6 @@
 // S2-PROJET-TANK-2.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
 //
-//:)
+
 
 #include <iostream>
 #include <windows.h> 
